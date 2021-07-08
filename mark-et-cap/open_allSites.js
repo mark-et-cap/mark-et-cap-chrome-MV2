bde154b806c4ed13b61c41b0e13c0fa2faf0cd0a
mark et cap dev <83081308+mark-et-cap@users.noreply.github.com>
2021-07-08 15:58:16 -0400

    v1.3 - open_allSites.js file added

---
+++ mark-et-cap/open_allSites.js
@@ -0,0 +1,21 @@
+let allSitesSymbol = '';
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, callback) {
+        if(request.message == "get_symbol" || request.message == "get_selection") {
+            allSitesSymbol = request.content;
+        } 
+    }
+);
+
+function allSitesSearch() { 
+    let screenWidth = window.screen.availWidth - 175;
+    recentInteraction(allSitesSymbol);
+    window.open('chrome-extension://lohldigbkedbcdcdmhobgffllmgkoekm/popup/mec-popup.html', "extension_popup", `top=67px,left=${screenWidth}px,width=385px,height=489px,status=no,scrollbars=no,resizable=no,titlebar=no`);
+    setTimeout(function(){
+        chrome.runtime.sendMessage({
+            content: allSitesSymbol.toUpperCase(), 
+            message: "open_search_page"
+        });
+    }, 1000);
+};
