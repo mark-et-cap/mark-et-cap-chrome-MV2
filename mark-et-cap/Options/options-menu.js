3377bdf92a3d738c3bf8cb7d9dc371e6a6f12f77
mark et cap dev <developer@mark-et-cap.com>
2022-03-02 18:15:05 -0500

    v2.4 - removing testing storage call

--- mark-et-cap/Options/options-menu.js
+++ mark-et-cap/Options/options-menu.js
@@ -25,55 +25,5 @@ function onloadWidgetSettings(){
 window.addEventListener('DOMContentLoaded',  function(){
     onloadWidgetSettings();
     startEventListener();
-    tempGetSavedOptions();
 })
 
-function tempGetSavedOptions() {
-    chrome.storage.sync.get([
-        "enableHoverChart",
-        "widgetSettingWidth",
-        "widgetSettingHeight",
-        "widgetSettingInterval",
-        "widgetSettingTimezone",
-        "widgetSettingTheme",
-        "widgetSettingBar",
-        "widgetSettingDate",
-        "widgetSettingDrawing",
-        "widgetSettingDetails",
-        "widgetSettingCalendar",
-        "widgetSettingPopup",
-        "widgetPopupWidth",
-        "widgetPopupHeight"
-    ], function(setting) {
-        let userWidgetChart = setting.enableHoverChart; 
-        let userWidgetWidth = setting.widgetSettingWidth;
-        let userWidgetHeight = setting.widgetSettingHeight;
-        let userWidgetInterval = setting.widgetSettingInterval;
-        let userWidgetTimezone = setting.widgetSettingTimezone;
-        let userWidgetTheme = setting.widgetSettingTheme;
-        let userWidgetBar = setting.widgetSettingBar; 
-        let userWidgetDate = setting.widgetSettingDate;
-        let userWidgetDrawing = setting.widgetSettingDrawing;
-        let userWidgetDetails = setting.widgetSettingDetails;
-        let userWidgetCalendar = setting.widgetSettingCalendar;
-        let userWidgetPopup = setting.widgetSettingPopup;
-        let userPopupWidth = setting.widgetPopupWidth;
-        let userPopupHeight = setting.widgetPopupHeight;
-
-
-        console.log(`userWidgetChart - ${userWidgetChart}`);
-        console.log(`userWidgetWidth - ${userWidgetWidth}`);
-        console.log(`userWidgetHeight - ${userWidgetHeight}`);
-        console.log(`userWidgetInterval - ${userWidgetInterval}`);
-        console.log(`userWidgetTimezone - ${userWidgetTimezone}`);
-        console.log(`userWidgetTheme - ${userWidgetTheme}`);
-        console.log(`userWidgetBar - ${userWidgetBar}`);
-        console.log(`userWidgetDate - ${userWidgetDate}`);
-        console.log(`userWidgetDrawing - ${userWidgetDrawing}`);
-        console.log(`userWidgetDetails - ${userWidgetDetails}`);
-        console.log(`userWidgetCalendar - ${userWidgetCalendar}`);
-        console.log(`userWidgetPopup - ${userWidgetPopup}`);
-        console.log(`userPopupWidth - ${userPopupWidth}`);
-        console.log(`userPopupHeight - ${userPopupHeight}`);
-    });
-};
