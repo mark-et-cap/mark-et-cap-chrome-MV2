9ce63dd58bce37dada24b2b660fb2ae7992db6fc
mark et cap dev <83081308+mark-et-cap@users.noreply.github.com>
2022-03-07 11:43:45 -0500

    v2.4 - removing legacy code

--- mark-et-cap/hover_tradingView.js
+++ mark-et-cap/hover_tradingView.js
@@ -75,39 +75,3 @@ function getNewWidget(tradingViewWidgetsymbol) {
     });
 };
 
-/**
- <!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div id="tradingview_278f6"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
-  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-  <script type="text/javascript">
-  new TradingView.widget(
-  {
-  "width": 550, // Yes
-  "height": 375, // Yes
-  "symbol": "NASDAQ:AAPL", // No
-  "interval": "15", // Yes
-  "timezone": "America/New_York", // Yes
-  "theme": "light", // Yes
-  "style": "1", // Yes
-  "locale": "en", // No
-  "toolbar_bg": "#f1f3f6",
-  "enable_publishing": true, //no
-  "withdateranges": true, // Yes (on/off)
-  "hide_side_toolbar": false, // Yes (on/off)
-  "allow_symbol_change": true, // No
-  "details": true, // Yes (on/off)
-  "hotlist": true, // No
-  "calendar": true, // Yes
-  "show_popup_button": true, // Yes
-  "popup_width": "1000", 
-  "popup_height": "650",
-  "container_id": "tradingview_278f6"
-}
-  );
-  </script>
-</div>
-<!-- TradingView Widget END -->
-
- */
